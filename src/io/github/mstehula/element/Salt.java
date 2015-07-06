b4c6942ab224c13ef0f910a578b2d872efd7bd74
mstehula <mstehula@gmail.com>
2015-07-06 14:26:32 -0500

    Element Added: Salt
    Soluble, making it so that when combined with water it makes Na+ and
    Cl-, which makes it useful for any chemical reactions.

---
+++ src/io/github/mstehula/element/Salt.java
@@ -0,0 +1,18 @@
+package io.github.mstehula.element;
+
+import io.github.mstehula.element.interfaces.Moveable;
+import io.github.mstehula.element.interfaces.Soluble;
+
+/**
+ * Created by MStehula on 7/6/2015.
+ */
+public class Salt extends AbstractElement implements Moveable, Soluble {
+
+    public void tickMoveable() {
+
+    }
+
+    public void tickSoluble() {
+
+    }
+}
