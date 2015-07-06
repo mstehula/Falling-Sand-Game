c71a2447e50014a73b57ae9497b94c18c8446f8a
mstehula <mstehula@gmail.com>
2015-07-06 14:16:43 -0500

    Simple modification to Water

--- src/io/github/mstehula/element/Water.java
+++ src/io/github/mstehula/element/Water.java
@@ -1,11 +1,13 @@
 package io.github.mstehula.element;
 
+import io.github.mstehula.element.interfaces.Liquid;
 import io.github.mstehula.element.interfaces.Moveable;
+import io.github.mstehula.element.interfaces.Solvent;
 
 /**
  * Created by MStehula on 7/6/2015.
  */
-public class Water extends AbstractElement implements Moveable, Liquid, Solvent{
+public class Water extends AbstractElement implements Moveable, Liquid, Solvent {
 
     public void tickMoveable() {
 
