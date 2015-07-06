0fe82ab8f13b617f5eaf5ad56e5d498defb46665
mstehula <mstehula@gmail.com>
2015-07-06 14:25:47 -0500

    Added Element: Oil
    Flamable, Moveable and Liquid. Burns slowly, with many impurities and
    pollution

---
+++ src/io/github/mstehula/element/Oil.java
@@ -0,0 +1,24 @@
+package io.github.mstehula.element;
+
+import io.github.mstehula.element.interfaces.Flammable;
+import io.github.mstehula.element.interfaces.Liquid;
+import io.github.mstehula.element.interfaces.Moveable;
+
+/**
+ * Created by MStehula on 7/6/2015.
+ */
+public class Oil extends AbstractElement implements Flammable, Liquid, Moveable {
+
+    public void tickFlammable() {
+
+    }
+
+    public void tickLiquid() {
+
+    }
+
+    public void tickMoveable() {
+
+    }
+
+}
