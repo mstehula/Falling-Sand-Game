c17bea53cec7dfe6b681b1c5d7d18550e823022f
mstehula <mstehula@gmail.com>
2015-07-06 14:27:20 -0500

    Element Added: Sand
    Sand simply sinks in water, and is inert. May intend to be heated into
    glass

---
+++ src/io/github/mstehula/element/Sand.java
@@ -0,0 +1,14 @@
+package io.github.mstehula.element;
+
+import io.github.mstehula.element.interfaces.Moveable;
+
+/**
+ * Created by MStehula on 7/6/2015.
+ */
+public class Sand extends AbstractElement implements Moveable {
+
+    public void tickMoveable() {
+
+    }
+
+}
