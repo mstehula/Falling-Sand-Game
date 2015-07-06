f151b2f2e1af11fd3d9d94e9a9249c34e56e2105
mstehula <mstehula@gmail.com>
2015-07-06 14:11:56 -0500

    Added Element: Water
    Added the element. Discovered the need for several new interfaces.

---
+++ src/io/github/mstehula/element/Water.java
@@ -0,0 +1,21 @@
+package io.github.mstehula.element;
+
+import io.github.mstehula.element.interfaces.Moveable;
+
+/**
+ * Created by MStehula on 7/6/2015.
+ */
+public class Water extends AbstractElement implements Moveable, Liquid, Solvent{
+
+    public void tickMoveable() {
+
+    }
+
+    public void tickLiquid() {
+
+    }
+
+    public void tickSolvent() {
+
+    }
+}
