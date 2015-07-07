4d4fecefdd4be6537cde6f511b565cc04cf94556
mstehula <mstehula@gmail.com>
2015-07-07 09:02:29 -0500

    Added Keyboard Listener
    If there is ever a decision to add keys, this will be it. Also used in
    the escape key to close the window and terminate the program.

---
+++ src/io/github/mstehula/controls/Keyboard.java
@@ -0,0 +1,22 @@
+package io.github.mstehula.controls;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+/**
+ * Created by MStehula on 7/7/2015.
+ */
+public class Keyboard implements KeyListener {
+
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    public void keyReleased(KeyEvent e) {
+
+    }
+}
