a3d2d34a8a22396d6c2817acb11d929a05baae5c
mstehula <mstehula@gmail.com>
2015-07-08 10:20:31 -0500

    Added stone for contrast of colors
    Testing to see if new colorizing works correctly

--- src/io/github/mstehula/elements/Stone.java
+++ src/io/github/mstehula/elements/Stone.java
@@ -1,9 +1,15 @@
 package io.github.mstehula.elements;
 
+import java.awt.*;
+
 /**
  * Created by MStehula on 7/6/2015.
  */
 public class Stone extends AbstractElement {
+
+    public Stone() {
+        this.color = new Color(0x646464);
+    }
 
     public void tick() {
 
